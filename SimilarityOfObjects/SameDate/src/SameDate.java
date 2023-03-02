d767ffc676e432aceb7020fd8e22d22eecedbfcc
Julio David <isaacdvid15.96@outlook.com>
2023-03-01 21:46:02 -0600

    update

---
+++ SimilarityOfObjects/SameDate/src/SameDate.java
@@ -0,0 +1,94 @@
+public class SameDate {
+    public static void main(String[] args) {
+        SimpleDate d = new SimpleDate(1, 2, 2000);
+        SimpleDate dee = new SimpleDate(1,2,2000);
+        System.out.println(d.equals("heh"));
+        System.out.println(d.equals(new SimpleDate(5, 2, 2012)));
+        System.out.println(d.equals(new SimpleDate(1, 2, 2000)));
+        System.out.println(d.equals(dee));
+        System.out.println(d.hashCode());
+        System.out.println(dee.hashCode());
+    }
+}
+
+class SimpleDate{
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(SimpleDate other) {
+        if (this.year < other.year) {
+            return true;
+        }
+
+        if (this.year == other.year
+                && this.month < other.month) {
+            return true;
+        }
+
+        if (this.year == other.year
+                && this.month == other.month
+                && this.day < other.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int differenceInYears(SimpleDate other) {
+        if (earlier(other)) {
+            return other.differenceInYears(this);
+        }
+
+        int yearRemoved = 0;
+
+        if (this.month < other.month) {
+            yearRemoved = 1;
+        } else if (this.month == other.month && this.day < other.day) {
+            yearRemoved = 1;
+        }
+
+        return this.year - other.year - yearRemoved;
+    }
+
+    public boolean equals(Object object){
+
+        if(this == object){
+            return true;
+        }
+
+        if(!(object instanceof SimpleDate)){
+            return false;
+        }
+
+        SimpleDate obj = (SimpleDate) object;
+
+        if(this.day == obj.day && this.month == obj.month && this.year == obj.year){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public int hashCode(){
+        int hash = 7;
+        hash = 17 * hash + this.day;
+        hash = 17 * hash + this.month;
+        hash = 17 * hash + this.year;
+        return hash;
+    }
+
+
+}
